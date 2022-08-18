ab2b1323c66537b045253a3d0a45995600e5fdc4
hamza umar <humar6078@gmail.com>
2022-08-18 21:21:58 +0000

    routes sorted

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,15 @@
 <?php
 
 use App\Http\Controllers\ClassesController;
+use App\Http\Controllers\DayController;
+use App\Http\Controllers\GradeController;
+use App\Http\Controllers\LevelController;
+use App\Http\Controllers\SessionController;
+use App\Http\Controllers\ShiftController;
+use App\Http\Controllers\SubjectController;
+use App\Http\Controllers\TimeController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -22,7 +19,13 @@ Auth::routes();
 
 Route::prefix('admin')->group(function () {
     Route::resource('classes', ClassesController::class);
-    Route::resource('grades');
+    Route::resource('grades', GradeController::class);
+    Route::resource('levels', LevelController::class);
+    Route::resource('days', DayController::class);
+    Route::resource('times', TimeController::class);
+    Route::resource('shifts', ShiftController::class);
+    Route::resource('subjects', SubjectController::class);
+    Route::resource('sessions', SessionController::class);
 });
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
