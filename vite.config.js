24a77221886c583d364bed085d65ab2dc45d40f8
hamza umar <humar6078@gmail.com>
2022-11-25 08:39:20 +0000

    ade akye

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
+import laravel, { refreshPaths } from "laravel-vite-plugin";
 
 export default defineConfig({
     // server: {
@@ -9,7 +9,8 @@ export default defineConfig({
     plugins: [
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
-            refresh: true,
+            // refresh: true,
+            refresh: [...refreshPaths, "app/Http/Livewire/**"],
         }),
     ],
 });
